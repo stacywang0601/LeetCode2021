c7452a16a1572c430f586edd2f854d01a9fc22ad
Stacy <stacy61c@gmail.com>
2021-05-08 16:33:21 -0400

    Serialize and Deserialize Binary Tree. Add bottom-to-up postorder approach

--- LeetCode2021/src/Leet297.java
+++ LeetCode2021/src/Leet297.java
@@ -13,7 +13,7 @@ public class Leet297 {
             val = x;
         }
     }
-    // DFS
+    // Approach 1: DFS with pre-order traverse: top-to-bottom, global sb return void
     // Time: O(n), where n is the number of nodes since we visit each node once
     // Space: O(n) since we keep the entire tree
 
@@ -47,6 +47,39 @@ public class Leet297 {
         TreeNode node = new TreeNode(Integer.valueOf(cur));
         node.left = deserialize(queue);
         node.right = deserialize(queue);
+        return node;
+    }
+
+
+    /**************************************************************************/
+    // Approach 2: DFS with post-order traverse: bottom-to-top, local sb, return string
+    // Encodes a tree to a single string.
+    public String serialize2(TreeNode node) {
+        if (node == null) {
+            return "#";
+
+        }
+        String left = serialize2(node.left);
+        String right = serialize2(node.right);
+        StringBuilder sb = new StringBuilder();
+        sb.append(node.val).append(",").append(left).append(",").append(right);
+        return sb.toString();
+    }
+
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize2(String data) {
+        return deserialize(new LinkedList<>(Arrays.asList(data.split(","))));
+    }
+
+    private TreeNode deserialize2(Queue<String> queue) {
+        String cur = queue.remove();
+        if (cur.equals("#")) {
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.parseInt(cur));
+        node.left = deserialize2(queue);
+        node.right = deserialize2(queue);
         return node;
     }
 }
