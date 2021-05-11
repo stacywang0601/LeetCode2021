a467248fab1671879f655b34ede5b6dba751f84b
Stacy <stacy61c@gmail.com>
2021-05-10 22:59:06 -0400

    Product of Array Except Self
    Temp and 2 loop

---
+++ LeetCode2021/src/Leet238.java
@@ -0,0 +1,21 @@
+/**
+ * 2021-05-10 Mon
+ * */
+public class Leet238 {
+    public int[] productExceptSelf(int[] nums) {
+        int temp = 1;
+        int[] res = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = temp;
+            temp *= nums[i];
+        }
+        temp = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            res[i] *= temp;
+            temp *= nums[i];
+        }
+        return res;
+
+    }
+}
