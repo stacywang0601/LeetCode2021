754ddc02144b6eb1b6f8c3266e13775227a0e01f
Stacy <stacy61c@gmail.com>
2021-05-11 21:25:34 -0400

    Other 3 Boundary Binary Search

--- LeetCode2021/src/Leet034.java
+++ LeetCode2021/src/Leet034.java
@@ -21,4 +21,45 @@ public class Leet034 {
         }
         return nums[l] >= target ? l : nums.length;
     }
+
+    /* Other 3 Boundary Binary Search */
+
+    private int less(int[] num, int k) {
+        int l = 0, h = num.length - 1;
+        while (l < h) {
+            int m = (l + h + 1) / 2;
+            if (num[m] < k) {
+                l = m;
+            } else {
+                h = m - 1;
+            }
+        }
+        return num[l] < k ? l : -1;
+    }
+
+    private int lessOrEqual(int[] num, int k) {
+        int l = 0, h = num.length - 1;
+        while (l < h) {
+            int m = (l + h + 1) / 2;
+            if (num[m] <= k) {
+                l = m;
+            } else {
+                h = m - 1;
+            }
+        }
+        return num[l] <= k ? l : -1;
+    }
+
+    private int greater(int[] num, int k) {
+        int l = 0, h = num.length - 1;
+        while (l < h) {
+            int m = (l + h) / 2;
+            if (num[m] > k) {
+                h = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return num[l] > k ? l : num.length;
+    }
 }
