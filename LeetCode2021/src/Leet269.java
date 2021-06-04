f8f37cd808df851f417cab2e5e2d26caea19957f
Stacy <stacy61c@gmail.com>
2021-06-03 20:24:45 -0400

    269. Alien Dictionary
    Only check first set of character that are not equal=

--- LeetCode2021/src/Leet269.java
+++ LeetCode2021/src/Leet269.java
@@ -2,17 +2,18 @@ import java.util.*;
 
 /**
  * 2021-06-01 Tue
- * */
+ */
 public class Leet269 {
     class Solution {
         public String alienOrder(String[] words) {
-            // Step1: Initialize
             List<Integer>[] adj = new ArrayList[26];
-            for (int i = 0; i < 26; i++) {
-                adj[i] = new ArrayList<Integer>();
-            }
             int[] degree = new int[26];
-            Arrays.fill(degree, -1);
+
+            // Step1: Initialize
+            for (int i = 0; i < 26; i++) {
+                adj[i] = new ArrayList<>();
+                degree[i] = -1;
+            }
 
             // Step2: Build the graph
             for (int i = 0; i < words.length; i++) {
@@ -21,49 +22,51 @@ public class Leet269 {
                         degree[c - 'a'] = 0;
                     }
                 }
+
                 if (i > 0) {
                     String w1 = words[i - 1], w2 = words[i];
                     int len = Math.min(w1.length(), w2.length());
                     for (int j = 0; j < len; j++) {
                         int c1 = w1.charAt(j) - 'a', c2 = w2.charAt(j) - 'a';
+                        // Only add when c1 != c2
                         if (c1 != c2) {
                             if (!adj[c1].contains(c2)) {
                                 adj[c1].add(c2);
                                 degree[c2]++;
                             }
+                            // Only check first set character, then break
                             break;
                         }
-                        // "abcd" -> "ab"
-                        if (j == w2.length() - 1 && w1.length() > w2.length()) {
-                            return "";
-                        }
+                        // len - 1
+                        if (j == w2.length() - 1 && w1.length() > w2.length()) return "";
                     }
                 }
             }
 
             // Step3: Topological Sort
-            Queue<Integer> q = new LinkedList<>();
-            for (int i = 0; i < degree.length; i++) {
+            Queue<Integer> queue = new LinkedList<>();
+            for (int i = 0; i < 26; i++) {
                 if (degree[i] == 0) {
-                    q.add(i);
+                    queue.add(i);
                 }
             }
 
             StringBuilder sb = new StringBuilder();
-            while (!q.isEmpty()) {
-                int i = q.poll();
-                sb.append((char) ('a' + i));
-                for (int j : adj[i]) {
-                    degree[j]--;
-                    if (degree[j] == 0) {
-                        q.add(j);
+            while (!queue.isEmpty()) {
+                int cur = queue.remove();
+                sb.append((char) (cur + 'a'));
+                for (int nei : adj[cur]) {
+                    degree[nei]--;
+                    if (degree[nei] == 0) {
+                        queue.add(nei);
                     }
                 }
             }
-            
+
             // Step 4: Check cyclic
-            for (int d : degree) {
-                if (d > 0) {
+            for (int i : degree) {
+                // have to be > 0, since initialize as -1
+                if (i > 0) {
                     return "";
                 }
             }
