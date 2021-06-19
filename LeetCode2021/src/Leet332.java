6eda1c630a176c4eb15bf708f246979e99fc918f
Stacy <stacy61c@gmail.com>
2021-06-19 10:53:44 -0400

    332. Reconstruct Itinerary
    RETURN!!!!

--- LeetCode2021/src/Leet332.java
+++ LeetCode2021/src/Leet332.java
@@ -1,35 +1,34 @@
 import java.util.*;
 /**
  * 2021-05-09 Sun
+ * 2021-06-19 Sat
  * */
 public class Leet332 {
-    public List<String> findItinerary(List<List<String>> tickets) {
-        List<String> res = new ArrayList<>();
-        Map<String, PriorityQueue<String>> map = new HashMap<>();
-
-        for(List<String> ticket: tickets) {
-            if(!map.containsKey(ticket.get(0))) {
-                map.put(ticket.get(0), new PriorityQueue<>());
+    class Solution {
+        public List<String> findItinerary(List<List<String>> tickets) {
+            Map<String, PriorityQueue<String>> map = new HashMap<>();
+            for(List<String> ticket: tickets) {
+                map.putIfAbsent(ticket.get(0), new PriorityQueue<>());
+                map.get(ticket.get(0)).add(ticket.get(1));
             }
-            map.get(ticket.get(0)).add(ticket.get(1));
+            List<String> res = new ArrayList<>();
+            dfs("JFK", map, res);
+            return res;
         }
 
-        dfs("JFK", res, map);
-        return res;
-    }
+        private void dfs(String depart, Map<String, PriorityQueue<String>> map, List<String> res) {
+            PriorityQueue<String> arrivals = map.get(depart);
 
-    private void dfs(String cur, List<String> res, Map<String, PriorityQueue<String>> map) {
-        PriorityQueue<String> arrivals = map.get(cur);
-        if(arrivals == null) {
-            // Stop condition: a node doesn't have any arrival, means it is the end. RETURN!
-            res.add(0, cur);
-            return;
+            if(arrivals == null) {
+                res.add(0, depart);
+                // !!!! return!!!!
+                return;
+            }
+
+            while(!arrivals.isEmpty()) {
+                dfs(arrivals.remove(), map, res);
+            }
+            res.add(0, depart);
         }
-        while(!arrivals.isEmpty()) {
-            // remove
-            dfs(arrivals.remove(), res, map);
-        }
-        // After getting back from the last recursion, all the arrivals are taken care of
-        res.add(0, cur);
     }
 }
