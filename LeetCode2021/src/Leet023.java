f0850b1debd0979e1e520549f73f88b3f1640f15
Stacy <stacy61c@gmail.com>
2021-06-26 20:47:00 -0400

    23. Merge k Sorted Lists

--- LeetCode2021/src/Leet023.java
+++ LeetCode2021/src/Leet023.java
@@ -53,22 +53,21 @@ public class Leet023 {
             return dummy.next;
         }
     }
-}
 
+    class ListNode {
+        int val;
+        ListNode next;
 
-class ListNode {
-    int val;
-    ListNode next;
+        ListNode() {
+        }
 
-    ListNode() {
-    }
+        ListNode(int val) {
+            this.val = val;
+        }
 
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
