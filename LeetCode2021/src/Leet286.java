b23fefa5a7362f7b4b66d8d77ee00b78042b548f
Stacy <stacy61c@gmail.com>
2021-07-01 10:28:00 -0400

    286. Walls and Gates
    Add DFS solution

--- LeetCode2021/src/Leet286.java
+++ LeetCode2021/src/Leet286.java
@@ -2,6 +2,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Leet286 {
+    // Multi-end BFS
     class Solution {
         int[] dir = {0, 1, 0, -1, 0};
 
@@ -28,6 +29,35 @@ public class Leet286 {
                     queue.add(new int[]{x, y});
                 }
             }
+        }
+    }
+
+    // DFS
+    class Solution2 {
+        int[] dir = {1, 0, -1, 0, 1};
+        int[][] rooms;
+        int m, n;
+        public void wallsAndGates(int[][] rooms) {
+            this.rooms = rooms;
+            this.m = rooms.length;
+            this.n = rooms[0].length;
+            for(int i = 0; i< m; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(rooms[i][j] == 0){
+                        dfs(i, j);
+                    }
+                }
+            }
+
+        }
+        private void dfs(int i, int j) {
+            for(int k = 0; k < 4; k++) {
+                int x = i + dir[k], y = j + dir[k+1];
+                if(x < 0 || x >= m || y < 0 || y >=n || rooms[x][y] <= rooms[i][j] + 1) continue;
+                rooms[x][y] = rooms[i][j] + 1;
+                dfs(x, y);
+            }
+
         }
     }
 }
