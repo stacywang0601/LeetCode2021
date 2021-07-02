d21a5770b2abd70a9333e65d3acb1390d15cc2be
Stacy <stacy61c@gmail.com>
2021-07-02 10:50:33 -0400

    1041. Robot Bounded In Circle

--- LeetCode2021/src/Leet1041.java
+++ LeetCode2021/src/Leet1041.java
@@ -5,7 +5,9 @@
 * If instruction is L, the next direction is i = (i - 1) % 4. To avoid negative indices, use i = (i + 3) % 4 instead
 * If instruction is to move, update x += directions[i][0], y += directions[i][1].
 
-* If the robot doesn't face north at the end of the first cycle, then that's the limit cycle trajectory.
+* If the robot FACE North at the end of the first cycle(not original point)
+* there DOESN'T exist a circle such that the robot never leaves the circle.
+*
 * After one cycle, the robot faces north.
 * Then here is what we have after 4 cycles:
 * x4=x+Δx+Δx−Δx+Δx=x+4Δx
@@ -24,19 +26,23 @@
 * y4=y+Δy+Δx−Δy−Δx=y
 */
 public class Leet1041 {
-    public boolean isRobotBounded(String instructions) {
-        int[][] dir = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-        int x = 0, y = 0, i = 0;
-        for(char ins: instructions.toCharArray()) {
-            if(ins == 'R') {
-                i = (i + 1) % 4;
-            }else if(ins == 'L') {
-                i = (i + 3) % 4;
-            }else {
-                x += dir[i][0];
-                y += dir[i][1];
+    class Solution {
+        public boolean isRobotBounded(String instructions) {
+            int[] dir = {0, 1, 0, -1, 0};
+            int x = 0, y = 0, i = 0;
+            for(char c: instructions.toCharArray()) {
+                if(c == 'R') {
+                    i = (i + 1) % 4;
+                }else if(c == 'L') {
+                    i = (i + 3) % 4;
+                }else {
+                    x += dir[i];
+                    y += dir[i+1];
+                }
             }
+            // robot returns into initial position
+            // or robot doesn't at initial position but doesn't face north
+            return (x == 0 && y == 0) || i != 0;
         }
-        return (x == 0 && y == 0) || i > 0;
     }
 }
