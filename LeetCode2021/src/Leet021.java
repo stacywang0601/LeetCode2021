d9cb5addcc881b458ca5c48f9e931a9fc78fff2f
Stacy <stacy61c@gmail.com>
2021-07-02 10:17:22 -0400

    21. Merge Two Sorted Lists

---
+++ LeetCode2021/src/Leet021.java
@@ -0,0 +1,57 @@
+public class Leet021 {
+    // Iterative with dummy node
+    class Solution {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            ListNode dummy = new ListNode(0);
+            ListNode pre = dummy;
+
+            while (l1 != null && l2 != null) {
+                if (l1.val < l2.val) {
+                    pre.next = l1;
+                    l1 = l1.next;
+                } else {
+                    pre.next = l2;
+                    l2 = l2.next;
+                }
+                pre = pre.next;
+            }
+
+            if (l1 == null) pre.next = l2;
+            if (l2 == null) pre.next = l1;
+            return dummy.next;
+        }
+    }
+
+    // Recursion 
+    class Solution2 {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if(l1 == null) return l2;
+            if(l2 == null)  return l1;
+
+            if(l1.val < l2.val) {
+                l1.next = mergeTwoLists(l1.next, l2);
+                return l1;
+            }
+
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
