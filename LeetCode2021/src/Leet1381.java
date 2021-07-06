b163e9021a87bf76524204e387bd946a8f6a01f9
Stacy <stacy61c@gmail.com>
2021-07-05 21:03:51 -0400

    1381. Design a Stack With Increment Operation
    int[] inc = new int[n+1]

--- LeetCode2021/src/Leet1381.java
+++ LeetCode2021/src/Leet1381.java
@@ -3,48 +3,47 @@ import java.util.Stack;
 /*
  * 2021-05-04 Tue
  * Use an additional array to record the increment value.
- * inc[i] means for all i+1 elements stack[0] ~ stack[i],
+ * inc[i] means for all i elements stack[0] ~ stack[i-1],
+ * total n elements, inc[n] --> so inc has length of n+1
  * We should plus inc[i] when popped from the stack.
  * then inc[i-1]+=inc[i],
  * so that we can accumulate the increment inc[i]
  * for the bottom elements and the following pops.
  */
-public class Leet1381 {
-    class CustomStack {
-        int[] inc;
-        int n;
-        Stack<Integer> stack;
+class CustomStack {
+    int n;
+    int[] inc;
+    Stack<Integer> stack;
 
-        public CustomStack(int maxSize) {
-            n = maxSize;
-            inc = new int[n];
-            stack = new Stack<>();
-        }
+    public CustomStack(int maxSize) {
+        this.n = maxSize;
+        stack = new Stack<>();
+        inc = new int[n + 1];
+    }
 
-        public void push(int x) {
-            if (stack.size() < n) {
-                stack.push(x);
-            }
-        }
-
-        public int pop() {
-            int i = stack.size() - 1;
-            if (i < 0) return -1;
-            if (i > 0) {
-                inc[i - 1] += inc[i];
-            }
-            int res = stack.pop() + inc[i];
-            inc[i] = 0;
-            return res;
-        }
-
-        public void increment(int k, int val) {
-            int i = Math.min(stack.size(), k) - 1;
-            if (i >= 0) {
-                inc[i] += val;
-            }
+    public void push(int x) {
+        if (stack.size() < n) {
+            stack.push(x);
         }
     }
+
+    public int pop() {
+        int i = stack.size();
+        if (i <= 0) {
+            return -1;
+        }
+
+        inc[i - 1] += inc[i];
+        int res = stack.pop() + inc[i];
+        inc[i] = 0;
+        return res;
+    }
+
+    public void increment(int k, int val) {
+        int i = Math.min(k, stack.size());
+        inc[i] += val;
+    }
+}
 
 /**
  * Your CustomStack object will be instantiated and called as such:
